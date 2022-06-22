07dcf09ffe3dc397a7da965964797d3b97173ee4
sina taherkhani <registry123456@gmail.com>
2022-06-22 11:20:28 +0430

    Update SelectionaryState.swift

--- Sources/SelectionaryStates/SelectionaryState.swift
+++ Sources/SelectionaryStates/SelectionaryState.swift
@@ -13,8 +13,8 @@ enum types{
 }
 class SelectionaryState: UIButton {
 
-    let checked = UIImage(named: "check")!
-    let uncheck = UIImage(named: "uncheck")!
+    public var checked = UIImage(named: "check")!
+    public var uncheck = UIImage(named: "uncheck")!
     
     public var isChecked:Bool = true
     public var type:types = .checkbox
